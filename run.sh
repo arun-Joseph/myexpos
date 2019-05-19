6a662951beed164cf95d360573f177712066d619
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-19 18:01:36 +0530

    Stage 4

--- run.sh
+++ run.sh
@@ -5,8 +5,12 @@ cd ../expl
 
 echo "SPL"
 cd ../spl
+./spl ./spl_progs/oddnos.spl
+./spl ./spl_progs/squaresum.spl
 
 echo "XFS Interface"
 cd ../xfs-interface
+./xfs-interface load --os ../spl/spl_progs/oddnos.xsm
+./xfs-interface load --os ../spl/spl_progs/squaresum.xsm
 
 echo "Completed."
