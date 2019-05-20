71d5325cb7020ecf21a0301d99fe53137d5b6842
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-20 12:54:31 +0530

    Stage 6

--- run.sh
+++ run.sh
@@ -5,11 +5,15 @@ cd ../expl
 
 echo "SPL"
 cd ../spl
-./spl ./spl_progs/debug.spl
+./spl ./spl_progs/os_startup.spl
+./spl ./spl_progs/haltprog.spl
 
 echo "XFS Interface"
 cd ../xfs-interface
 ./xfs-interface fdisk
-./xfs-interface load --os ../spl/spl_progs/debug.xsm
+./xfs-interface load --os ../spl/spl_progs/os_startup.xsm
+./xfs-interface load --exhandler ../spl/spl_progs/haltprog.xsm
+./xfs-interface load --int=10 ../spl/spl_progs/haltprog.xsm
+./xfs-interface load --init ../expl/expl_progs/squares.xsm
 
 echo "Completed."
