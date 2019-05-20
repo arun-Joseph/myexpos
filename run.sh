975a524368a2498f65ec0077750f43efbead5c57
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-20 13:22:14 +0530

    Stage 8

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ cd ../expl
 echo "SPL"
 cd ../spl
 ./spl ./spl_progs/os_startup.spl
+./spl ./spl_progs/sample_timer.spl
 ./spl ./spl_progs/haltprog.spl
 
 echo "XFS Interface"
@@ -13,6 +14,7 @@ cd ../xfs-interface
 ./xfs-interface fdisk
 ./xfs-interface load --os ../spl/spl_progs/os_startup.xsm
 ./xfs-interface load --exhandler ../spl/spl_progs/haltprog.xsm
+./xfs-interface load --int=timer ../spl/spl_progs/sample_timer.xsm
 ./xfs-interface load --int=10 ../spl/spl_progs/haltprog.xsm
 ./xfs-interface load --library ../expl/library.lib
 ./xfs-interface load --init ../expl/expl_progs/init.xsm
