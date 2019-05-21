f2babbde5387fa2c26fe61f496caac6a520ba057
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-21 21:52:07 +0530

    Stage 13

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ cd ../spl
 ./spl ./spl_progs/os_startup.spl
 ./spl ./spl_progs/timer.spl
 ./spl ./spl_progs/sample_int7.spl
+./spl ./spl_progs/mod7.spl
 ./spl ./spl_progs/haltprog.spl
 
 echo "XFS Interface"
@@ -22,6 +23,7 @@ cd ../xfs-interface
 ./xfs-interface load --int=timer ../spl/spl_progs/timer.xsm
 ./xfs-interface load --int=7 ../spl/spl_progs/sample_int7.xsm
 ./xfs-interface load --int=10 ../spl/spl_progs/haltprog.xsm
+./xfs-interface load --module 7 ../spl/spl_progs/mod7.xsm
 ./xfs-interface load --library ../expl/library.lib
 ./xfs-interface load --init ../expl/expl_progs/numbers.xsm
 ./xfs-interface load --idle ../expl/expl_progs/idle.xsm
