13fe713160346395340ccb68d855ade4cf0bfcff
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-22 11:19:17 +0530

    Stage 14

--- run.sh
+++ run.sh
@@ -4,14 +4,16 @@ echo "ExpL"
 cd ../expl
 ./expl ./expl_progs/idle.expl
 ./expl ./expl_progs/numbers.expl
-./expl ./expl_progs/nums1.expl
-./expl ./expl_progs/nums2.expl
+./expl ./expl_progs/even.expl
+./expl ./expl_progs/odd.expl
 
 echo "SPL"
 cd ../spl
 ./spl ./spl_progs/os_startup.spl
 ./spl ./spl_progs/timer.spl
 ./spl ./spl_progs/sample_int7.spl
+./spl ./spl_progs/int10.spl
+./spl ./spl_progs/mod5.spl
 ./spl ./spl_progs/mod7.spl
 ./spl ./spl_progs/haltprog.spl
 
@@ -22,11 +24,13 @@ cd ../xfs-interface
 ./xfs-interface load --exhandler ../spl/spl_progs/haltprog.xsm
 ./xfs-interface load --int=timer ../spl/spl_progs/timer.xsm
 ./xfs-interface load --int=7 ../spl/spl_progs/sample_int7.xsm
-./xfs-interface load --int=10 ../spl/spl_progs/haltprog.xsm
+./xfs-interface load --int=10 ../spl/spl_progs/int10.xsm
+./xfs-interface load --module 5 ../spl/spl_progs/mod5.xsm
 ./xfs-interface load --module 7 ../spl/spl_progs/mod7.xsm
 ./xfs-interface load --library ../expl/library.lib
-./xfs-interface load --init ../expl/expl_progs/numbers.xsm
+./xfs-interface load --init ../expl/expl_progs/odd.xsm
 ./xfs-interface load --idle ../expl/expl_progs/idle.xsm
+./xfs-interface load --exec ../expl/expl_progs/even.xsm
 
 
 echo "Completed."
