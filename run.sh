cf2a1bc1594caa796d2e4123167b2fc0a9e6b041
Arun Joseph <arunjosephkv2@gmail.com>
2019-05-29 21:03:11 +0530

    Stage 28

--- run.sh
+++ run.sh
@@ -40,8 +40,9 @@ cd ../expl
 ./expl ./expl_progs/m_merge.expl
 
 echo "SPL"
-cd ../spl
+cd ../nespl
 ./spl ./spl_progs/os_startup.spl
+./spl ./spl_progs/os2_startup.spl
 ./spl ./spl_progs/exception.spl
 ./spl ./spl_progs/timer.spl
 ./spl ./spl_progs/disk.spl
@@ -68,37 +69,40 @@ cd ../spl
 ./spl ./spl_progs/mod5.spl
 ./spl ./spl_progs/mod6.spl
 ./spl ./spl_progs/mod7.spl
+./spl ./spl_progs/mod8.spl
 
 echo "XFS Interface"
-cd ../xfs-interface
+cd ../nexfs-interface
 ./xfs-interface fdisk
-./xfs-interface load --os ../spl/spl_progs/os_startup.xsm
-./xfs-interface load --exhandler ../spl/spl_progs/exception.xsm
-./xfs-interface load --int=timer ../spl/spl_progs/timer.xsm
-./xfs-interface load --int=disk ../spl/spl_progs/disk.xsm
-./xfs-interface load --int=console ../spl/spl_progs/console.xsm
-./xfs-interface load --int=4 ../spl/spl_progs/int4.xsm
-./xfs-interface load --int=5 ../spl/spl_progs/int5.xsm
-./xfs-interface load --int=6 ../spl/spl_progs/int6.xsm
-./xfs-interface load --int=7 ../spl/spl_progs/int7.xsm
-./xfs-interface load --int=8 ../spl/spl_progs/int8.xsm
-./xfs-interface load --int=9 ../spl/spl_progs/int9.xsm
-./xfs-interface load --int=10 ../spl/spl_progs/int10.xsm
-./xfs-interface load --int=11 ../spl/spl_progs/int11.xsm
-./xfs-interface load --int=12 ../spl/spl_progs/int12.xsm
-./xfs-interface load --int=13 ../spl/spl_progs/int13.xsm
-./xfs-interface load --int=14 ../spl/spl_progs/int14.xsm
-./xfs-interface load --int=15 ../spl/spl_progs/int15.xsm
-./xfs-interface load --int=16 ../spl/spl_progs/int16.xsm
-./xfs-interface load --int=17 ../spl/spl_progs/int17.xsm
-./xfs-interface load --module 0 ../spl/spl_progs/mod0.xsm
-./xfs-interface load --module 1 ../spl/spl_progs/mod1.xsm
-./xfs-interface load --module 2 ../spl/spl_progs/mod2.xsm
-./xfs-interface load --module 3 ../spl/spl_progs/mod3.xsm
-./xfs-interface load --module 4 ../spl/spl_progs/mod4.xsm
-./xfs-interface load --module 5 ../spl/spl_progs/mod5.xsm
-./xfs-interface load --module 6 ../spl/spl_progs/mod6.xsm
-./xfs-interface load --module 7 ../spl/spl_progs/mod7.xsm
+./xfs-interface load --os=primary ../nespl/spl_progs/os_startup.xsm
+./xfs-interface load --os=secondary ../nespl/spl_progs/os2_startup.xsm
+./xfs-interface load --exhandler ../nespl/spl_progs/exception.xsm
+./xfs-interface load --int=timer ../nespl/spl_progs/timer.xsm
+./xfs-interface load --int=disk ../nespl/spl_progs/disk.xsm
+./xfs-interface load --int=console ../nespl/spl_progs/console.xsm
+./xfs-interface load --int=4 ../nespl/spl_progs/int4.xsm
+./xfs-interface load --int=5 ../nespl/spl_progs/int5.xsm
+./xfs-interface load --int=6 ../nespl/spl_progs/int6.xsm
+./xfs-interface load --int=7 ../nespl/spl_progs/int7.xsm
+./xfs-interface load --int=8 ../nespl/spl_progs/int8.xsm
+./xfs-interface load --int=9 ../nespl/spl_progs/int9.xsm
+./xfs-interface load --int=10 ../nespl/spl_progs/int10.xsm
+./xfs-interface load --int=11 ../nespl/spl_progs/int11.xsm
+./xfs-interface load --int=12 ../nespl/spl_progs/int12.xsm
+./xfs-interface load --int=13 ../nespl/spl_progs/int13.xsm
+./xfs-interface load --int=14 ../nespl/spl_progs/int14.xsm
+./xfs-interface load --int=15 ../nespl/spl_progs/int15.xsm
+./xfs-interface load --int=16 ../nespl/spl_progs/int16.xsm
+./xfs-interface load --int=17 ../nespl/spl_progs/int17.xsm
+./xfs-interface load --module 0 ../nespl/spl_progs/mod0.xsm
+./xfs-interface load --module 1 ../nespl/spl_progs/mod1.xsm
+./xfs-interface load --module 2 ../nespl/spl_progs/mod2.xsm
+./xfs-interface load --module 3 ../nespl/spl_progs/mod3.xsm
+./xfs-interface load --module 4 ../nespl/spl_progs/mod4.xsm
+./xfs-interface load --module 5 ../nespl/spl_progs/mod5.xsm
+./xfs-interface load --module 6 ../nespl/spl_progs/mod6.xsm
+./xfs-interface load --module 7 ../nespl/spl_progs/mod7.xsm
+./xfs-interface load --module 8 ../nespl/spl_progs/mod8.xsm
 ./xfs-interface load --library ../expl/library.lib
 ./xfs-interface load --init ../expl/expl_progs/login.xsm
 ./xfs-interface load --shell ../expl/expl_progs/shell.xsm
